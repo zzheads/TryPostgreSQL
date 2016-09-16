e5016ad274babf4c392f35ab6b3e7c34643d46ab
Alexey Papin <zzheads@gmail.com>
2016-09-16 12:03:28 +0300

    Initial commit

---
+++ src/main/java/com/zzheads/trypostgresql/config/DataConfig.java
@@ -0,0 +1,28 @@
+package com.zzheads.trypostgresql.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class DataConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
